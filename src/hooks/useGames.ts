7d5794a3e85f763796fa7063ee5a709e55376217
Clayson <clay.fell8@gmail.com>
2023-07-14 10:41:32 -0700

    Implemented infinite scrolling.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -31,7 +31,8 @@ const useGames = (gameQuery: GameQuery) =>
         }),
         getNextPageParam: (lastpage, allpages) => {
           return lastpage.next ?  allpages.length + 1 : undefined;
-        }
+        },
+        staleTime: 24 * 60 * 60 * 1000 //24h
     })
 
 export default useGames;
